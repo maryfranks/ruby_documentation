c2464322ba2cb013341e839105d8ddd3e792f976
maryfranks <maryfranks@gmail.com>
2017-07-01 15:13:54 -0400

    completed exercise

---
+++ documentation.rb
@@ -0,0 +1,39 @@
+# Notes from the Ruby Docs
+
+# # Strings
+
+# length
+
+# strip
+
+# split
+
+# start_with?
+
+# # Arrays
+
+# first
+
+# delete_at
+
+# delete
+
+# pop
+
+# # Hash
+
+# to_a
+
+# has_key?
+
+# has_value?
+
+# # Time
+
+# now
+
+# # File
+
+# exist?
+
+# extname
